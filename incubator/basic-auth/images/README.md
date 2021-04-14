ad3ae9dace85b449e4a142f7e8b63fed8a8b3c8a
Emerson Ford <emersontford@gmail.com>
2021-04-14 15:51:35 -0600

    Remove chart information

--- incubator/basic-auth/images/README.md
+++
@@ -1,2 +0,0 @@
-# container-basic-auth
-This is generalized container meant to support basic Authentication and SSH through SSSD integration to AD/Kerberos/LDAP
--- stable/condor-worker/images/condor-worker/container-files/etc/osg/image-config.d/fix-perms.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p /etc/condor/tokens.d
-cp -a /root/tokens/* /etc/condor/tokens.d/
-chown -R condor: /etc/condor/tokens.d
-chmod 644 /etc/condor/tokens.d/*
