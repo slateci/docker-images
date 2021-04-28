5311472c4de0a1a219d382b1d4ec52bee27a85f2
Emerson Ford <emersontford@gmail.com>
2021-04-28 17:32:37 -0600

    Change image removal to just image prune

--- build.py
+++ build.py
@@ -218,24 +218,30 @@ def build_folder(folder: str, metadata: Dict[str, Any], tags: List[str]) -> bool
         return False
 
     # Delete all existing Docker images.
-    image_list = subprocess.run(["docker", "images", "-a", "-q"], capture_output=True)
+    # image_list = subprocess.run(["docker", "images", "-a", "-q"], capture_output=True)
 
-    if image_list.returncode != 0:
-        gh_error("Failed to get image list for cleanup!")
-        return False
+    # if image_list.returncode != 0:
+    #     gh_error("Failed to get image list for cleanup!")
+    #     return False
 
-    image_clean = all(
-        map(
-            lambda x: subprocess.run(
-                ["docker", "rmi", "-f", x], stdout=stdout
-            ).returncode
-            == 0,
-            image_list.stdout.decode().splitlines(),
-        )
-    )
+    # image_clean = all(
+    #     map(
+    #         lambda x: subprocess.run(
+    #             ["docker", "rmi", "-f", x], stdout=stdout
+    #         ).returncode
+    #         == 0,
+    #         image_list.stdout.decode().splitlines(),
+    #     )
+    # )
 
-    if not image_clean:
-        gh_error("Failed to clean image cache!")
+    # if not image_clean:
+    #     gh_error("Failed to clean image cache!")
+    #     return False
+
+    image_clean = subprocess.run(["docker", "image", "prune", "-f"])
+
+    if image_clean.returncode != 0:
+        gh_error("Failed to prune images!")
         return False
 
     # Pull image caches.
