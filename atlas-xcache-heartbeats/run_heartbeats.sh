5087898ca73ef213479ef01112dbfddb96239c40
Adam Griffith <94396076+adamhgriffith-uofu@users.noreply.github.com>
2023-06-16 12:44:19 -0600

    New atlas-xcache-heartbeats image (#228)
    * Content dump for atlas-xcache-heartbeats from https://github.com/ivukotic/docker-xcache/tree/master/atlas-xcache/Dockerfile.heartbeats

---
+++ atlas-xcache-heartbeats/run_heartbeats.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+size=`df -l | grep xcache/data | awk '{sum+=$2;} END{print sum;}'`
+
+# sleep until x509 things set up.
+while [ ! -f $X509_USER_PROXY ]
+do
+  sleep 10
+  echo "waiting for x509 proxy."
+done
+
+ls -lh $X509_USER_PROXY
+
+
+rucio whoami
+
+while true; do
+  date
+
+  # echo 'checking xcache'
+  # change to actual server address.
+  RESULT=$(xrdfs $ADDRESS query config sitename)
+  # could do more
+  # eg. xrdfs 192.170.227.122:1094 query stats a
+
+  # echo "Site is $RESULT. Expected $XC_RESOURCENAME"
+  if [ $RESULT == $XC_RESOURCENAME ]; then
+    # echo "Sending Rucio heartbeat"
+    # echo "needs: $XC_RESOURCENAME, $XC_INSTANCE, $ADDRESS, $size"
+    python3 /usr/local/sbin/heartbeat.py $XC_RESOURCENAME, $XC_INSTANCE, $ADDRESS, $size
+  else
+    echo "Something is wrong."
+  fi
+
+  sleep $FREQUENCY
+done
